d38c83894f04577bfdf7493384eaa76747b83e38
aobboa004 <aobboa004@gmail.com>
2020-03-07 03:38:30 +0530

    event admin full working

---
+++ app/src/main/java/com/learning/eventadmin/EventsAdapter.java
@@ -0,0 +1,70 @@
+package com.learning.eventadmin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+public class EventsAdapter extends ArrayAdapter<Events> {
+
+    private Activity context;
+    private List<Events> list;
+    private DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Events");;
+
+    public EventsAdapter(Activity context,List<Events> list)
+    {   super(context,R.layout.single_event_info_layout,list);
+        this.context=context;
+        this.list=list;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater=context.getLayoutInflater();
+        View listviewitem=layoutInflater.inflate(R.layout.single_event_info_layout,null,true);
+        TextView NameD=(TextView)listviewitem.findViewById(R.id.NameD);
+        TextView OrganiserD=(TextView)listviewitem.findViewById(R.id.OrganiserD);
+        TextView VenueD=(TextView)listviewitem.findViewById(R.id.VenueD);
+        TextView StartdateD=(TextView)listviewitem.findViewById(R.id.StartdateD);
+        TextView StarttimeD=(TextView)listviewitem.findViewById(R.id.StarttimeD);
+        TextView EnddateD=(TextView)listviewitem.findViewById(R.id.EnddateD);
+        TextView PhoneD=(TextView)listviewitem.findViewById(R.id.phoneD);
+        TextView StatusD=(TextView)listviewitem.findViewById(R.id.statusD);
+
+
+
+        final Events event=list.get(position);
+
+        NameD.setText(event.getName());
+        OrganiserD.setText(event.getOrganiser());
+        VenueD.setText(event.getVenue());
+        StartdateD.setText(event.getStartdate());
+        StarttimeD.setText(event.getStarttime());
+        EnddateD.setText(event.getEnddate());
+        PhoneD.setText(event.getContactinfo());
+        StatusD.setText(event.getStatus());
+
+
+
+
+
+        return listviewitem;
+    }
+
+
+}
--- app/src/main/java/com/learning/eventadmin/MainActivity.java
+++ app/src/main/java/com/learning/eventadmin/MainActivity.java
@@ -1,24 +1,58 @@
 package com.learning.eventadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    Button createEvent;
+    Button viewEvents;
+    EditText eventStartingDate;
+    EditText eventEndingDate;
+    EditText eventName;
+    EditText eventVenue;
+    EditText eventOrganisedBy;
+    EditText eventTime;
+    EditText contactInfo;
+    FirebaseDatabase database;
+    DatabaseReference ref;
     FirebaseAuth mAuth;
     Button btnLogout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createEvent=findViewById(R.id.createEvent);
+        viewEvents=findViewById(R.id.viewEvents);
+        eventStartingDate=findViewById((R.id.eventDateFrom));
+        eventEndingDate=findViewById(R.id.eventDateTo);
+        eventName=findViewById(R.id.eventName);
+        eventVenue=findViewById(R.id.eventVenue);
+        eventOrganisedBy=findViewById(R.id.eventOrganisedBy);
+        eventTime=findViewById(R.id.eventStartingTime);
+        contactInfo=findViewById(R.id.eventContactInfo);
+        database=FirebaseDatabase.getInstance();
+        ref=database.getReference("Events");
         mAuth = FirebaseAuth.getInstance();
-        btnLogout=(Button)findViewById(R.id.logout);
+        btnLogout=(Button)findViewById(R.id.log);
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,5 +61,90 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intToMain);
             }
         });
+
+        createEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEvent();
+            }
+        });
+
+
+    }
+
+    private void addEvent() {
+        String name=eventName.getText().toString();
+        String venue=eventVenue.getText().toString();
+        String organisedby=eventOrganisedBy.getText().toString();
+        String startdate=eventStartingDate.getText().toString();
+        String enddate=eventEndingDate.getText().toString();
+        String starttime=eventTime.getText().toString();
+        String contactinfo=contactInfo.getText().toString();
+
+        if(name.isEmpty())
+        {
+            eventName.setError("Name is required");
+            eventName.requestFocus();
+            return;
+        }
+
+        if(venue.isEmpty())
+        {
+            eventVenue.setError("Venue is required");
+            eventVenue.requestFocus();
+            return;
+        }
+
+        if(organisedby.isEmpty())
+        {
+            eventOrganisedBy.setError("Organiser Name is required");
+            eventOrganisedBy.requestFocus();
+            return;
+        }
+
+        if(startdate.isEmpty())
+        {
+            eventStartingDate.setError("Start date is required");
+            eventStartingDate.requestFocus();
+            return;
+        }
+
+        if(enddate.isEmpty())
+        {
+            eventEndingDate.setError("End date is required");
+            eventEndingDate.requestFocus();
+            return;
+        }
+
+        if(starttime.isEmpty())
+        {
+            eventTime.setError("Start Time is required");
+            eventTime.requestFocus();
+            return;
+        }
+
+        if(contactinfo.isEmpty())
+        {
+            contactInfo.setError("Contact Info is required");
+            contactInfo.requestFocus();
+            return;
+        }
+
+        String id=ref.push().getKey();
+        Toast toast=Toast.makeText(MainActivity.this,"Request Sent",Toast.LENGTH_SHORT);
+        toast.show();
+
+        Events event=new Events(name,id,organisedby,startdate,enddate,starttime,contactinfo,venue);
+        ref.child(id).setValue(event);
+    }
+
+
+
+
+
+    public void btnDisplay(View view) {
+        Intent intent=new Intent(MainActivity.this,Display.class);
+        startActivity(intent);
     }
 }
+
